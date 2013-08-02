f9405fc2bc3c2a11473293a61b2e2898b7a75681
mabdi <m.abdi@aut.ac.ir>
2013-08-02 16:37:38 +0430

    final

--- ids.rb
+++ ids.rb
@@ -1,8 +1,6 @@
 require 'bindata'
-require "readline" # for debugging porpuse
 
 #----------- CONSTANTS
-
 DATASET = "wc_day58_3"
 PERIOD = 1 # period is 1 sec
 K1 = 5 # MAX SIZE OF @pkt 
@@ -90,7 +88,6 @@ class FlashCrowd
    def etime
 	return @etime
    end
-
    def nxt time
 	return @pkt.select {|p| p.timestamp_ >= time && p.timestamp_ < time + PERIOD}
    end
@@ -100,7 +97,6 @@ class BotNet
 	@bots = (startID..(startID + n)).map{|x| [x, BOTACTIVEFROM + rand(BOTACTIVERAND).to_i]}
 	const startTime,endTime
    end
-
    def const startTime,endTime
 	@pkt = Array.new
 	for i in startTime..endTime
@@ -142,17 +138,10 @@ class DSRequest
 	if c.nan? then c = 1 end
 	return c
   end
-  def method1
-	@x = @pkt.values
-	@y = @pkt.keys	
-  end
-  def method2
-  	@x = @pkt.values.odd_values
-	@y = @pkt.values.even_values
-  end
   def process
      r =0
-     method1
+     @x = @pkt.values
+     @y = @pkt.keys
      r = (calcCorrel @x,@y).abs
      @pkt.delete 0
      @p.push r
@@ -189,37 +178,10 @@ class DSRequest
         process 
      end
   end
-  def get_x
-	return @x
-  end
-  def get_y
-	return @y
-  end
-  def get_pkt
-	return @pkt
-  end
-  def get_p
-	return @p
-  end
 end
 #---------- FUNCTIONS
 def set_red s
 	return "#{RED}#{s}#{COLOREND}"
-end
-def set_blue s
-        return "#{BLUE}#{s}#{COLOREND}"
-end
-def set_yellow s
-        return "#{YELLOW}#{s}#{COLOREND}"
-end
-def set_bold s
-        return "#{BOLD}#{s}#{BOLDEND}"
-end
-def d(s)
-	color_s = "\033[1m\033[33m"
-	color_f = "\033[0m\033[22m"
-	line= (caller.first.split ":")[1]
-	puts  "#{color_s}#{Time.new.strftime("%H:%M:%S")} line:#{line} -- #{s.to_s}#{color_f}"
 end
 def l(n,s)
    if n <= LOGLEVEL then
@@ -229,51 +191,6 @@ def l(n,s)
 	puts  "#{color_s}#{Time.new.strftime("%H:%M:%S")} #{space} #{s.to_s}#{color_f}"
    end
 end
-def e(n,s)
-	color_s = "\033[1m\033[31m"
-	color_f = "\033[0m\033[22m"
-	space = "   " *n
-	puts  "#{color_s}#{Time.new.strftime("%H:%M:%S")} #{space} #{s.to_s}#{color_f}"
-end
-def b bind
-        color_s = "\033[1m\033[31m"
-        color_f = "\033[0m\033[22m"
-        line= (caller.first.split ":")[1]
-	vars = (eval('local_variables',bind) | eval('instance_variables',bind)).map{|v| "#{v.to_s}= #{eval(v.to_s,bind)}"}.join ";"
-        puts  "#{color_s}#{Time.new.strftime("%H:%M:%S")} line:#{line} -- #{vars}#{color_f}"
-	begin
-		print "\033[31m"
-	        begin
-		  s = Readline.readline("dbg> ",true).strip
-	          if s == "" then break end
-	          eval ("puts ' = ' + (#{s}).to_s"),bind
-		rescue => e
-		  puts " > Error ocurred: \033[1m#{e.backtrace[0]}: #{e.message}\033[22m"
-	        end while true
-	ensure
-		print "\033[0m"
-	end
-end
-def x bind
-        color_s = "\033[1m\033[33m"
-        color_f = "\033[0m\033[22m"
-        puts  "#{color_s}Execuation mode: #{color_f}"
-        begin
-                print "\033[33m"
-                begin
-                  s = Readline.readline("exe> ",true).strip
-                  if s == "" then break end
-                  eval ("puts ' = ' + (#{s}).to_s"),bind
-                rescue => e
-                  puts " > Error ocurred: \033[1m#{e.backtrace[0]}: #{e.message}\033[22m"
-                end while true
-        ensure
-                print "\033[0m"
-        end
-end
-def h_get hash_,key_
-  return hash_[key_]
-end 
 $allowed = 0
 def drop req
  $allowed+=1	
